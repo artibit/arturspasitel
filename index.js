ddab23eeb227abd6eb19aad87a7a3e0273c6cf08
artibit <57136737+artibit@users.noreply.github.com>
2020-03-18 18:08:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,11 +8,43 @@ var mysql = require("mysql2");
 
 var http = require('http');
 var server = http.Server(app);
+app.post("/kek_action", upload.array(),function(request, response,next){
+    console.log(request);
+    console.log(request.body);
+    console.log(request.body.name);
+    connection.execute(
+      "INSERT INTO keker(field-name-1,field-email,field-date,radio-group-1,field-name-4,field-name-2,se) VALUES (?,?,?,?,?,?,?)",
+      [request.body.field-name-1, request.body.field-email,request.body.field-date,request.body.radio-group-1,request.body.field-name-4,request.body.field-name-2,request.body.se],
+      function (err, results, fields) {
+          console.log(results);
+          console.log(fields);
+          if (err) {
+              return console.error("Ошибка: " + err.message);
+          }
+      }
+  );
+    return response.redirect("/form");
+});
 
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
- 
-app.use("/form", function(request, response){
+
+const connection = mysql.createConnection({
+    host: "b4free.net",
+    user: "artibit",
+    database: "kekero",
+    password: "spageti1842"
+  });
+  connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+
+app.use("/form",express.static(__dirname + '/public'), function(request, response){
      
     return response.render("form", {
         title: "Мои контакты",
